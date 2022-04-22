1ff5e51f4d7cf42edb45b708c16eb0ed7e5bdcd6
hayeonkimm <hayeon.data@gmail.com>
2022-04-22 15:27:00 +0900

    로그아웃 기능추가

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { User } = require("./models/User");
 const cookieParser = require('cookie-parser');
 
 
+const {auth} = require('./middleware/auth');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -49,7 +51,7 @@ app.post('/register', (req, res)=>{
 })
 
 
-app.post('/login', (req,res)=>{
+app.post('/api/users/login', (req,res)=>{
 
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user)=>{
@@ -82,6 +84,49 @@ app.post('/login', (req,res)=>{
     })
   })
 })
+
+
+
+
+app.get('/api/users/auth', auth, (req, res)=>{
+
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는  Authentication이 True라는 말.
+  res.status(200).json({
+
+
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAtuth: true,
+    email : req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+
+  })
+
+
+  
+})
+
+
+
+
+
+
+app.get('/api/users/logout', auth, (req,res)=>{
+
+  User.findOneAndUpdate({_id: req.user._id},
+    { token: ""}
+    , (err, user)=>{
+      if (err) return res.json({ success: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
+
+})
+
 
 
 app.listen(port, () => {
