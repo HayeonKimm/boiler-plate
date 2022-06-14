a19fd167632ba7869e33f9d57aa8d713f5330631
hayeonkimm <hayeon.data@gmail.com>
2022-06-14 19:05:11 +0900

    업데이트

--- server/index.js
+++ server/index.js
@@ -1,139 +1,109 @@
-const express = require('express')
-const req = require('express/lib/request')
-const app = express()
+const express = require('express');
+const req = require('express/lib/request');
+const app = express();
 const bodyParser = require('body-parser');
 const config = require('./config/key');
-const { User } = require("./models/User");
+const { User } = require('./models/User');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
+const { auth } = require('./middleware/auth');
 
-const {auth} = require('./middleware/auth');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 
 const mongoose = require('mongoose');
 const { is } = require('express/lib/request');
-mongoose.connect(config.mongoURI,{
-
-  useNewUrlParser:true, useUnifiedTopology:true
-
-
-}).then(()=> console.log('몽고디비가 연결되었다아리마스!!!!'))  
-    .catch(err => console.log(err))
-
-
-
-app.get('/', (req, res) =>  res.send('안녕하세요 저는 김하연입니다.'))
-
-app.get('/api/hello', (req,res) => {
-  res.send("안녕하세요~")
-})
-
-
-app.post('/register', (req, res)=>{
-
-
-// 회원가입 할 때 필요한 정보들을 client에서 가져오면 
-// 그것들을 데이터 베이스에 넣어준다. 
-
-
-
-  const user = new User(req.body)
-
-  user.save((err, userInfo)=>{
-    if (err) return res.json({ success: false,err})
-    return res.status(200).json({
-      success:true
-    })
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
+  .then(() => console.log('몽고디비가 연결되었다아리마스!!!!'))
+  .catch((err) => console.log(err));
 
-})
+app.get('/', (req, res) => res.send('안녕하세요 저는 김하연입니다.'));
 
+app.get('/api/hello', (req, res) => {
+  res.send('Hello world!!!');
+});
 
-app.post('/api/users/login', (req,res)=>{
+app.post('/register', (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
 
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post('/api/users/login', (req, res) => {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne({ email: req.body.email }, (err, user)=>{
-    if(!user) {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
       return res.json({
         loginSuccess: false,
-        message: "제공된 이메일에 해당하는 유저가 없습니다."
-      })
+        message: '제공된 이메일에 해당하는 유저가 없습니다.',
+      });
     }
- 
 
-  //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
 
-    user.comparePassword(req.body.password, (err,isMatch)=>{
-      if(!isMatch)
-        return res.json({ loginSuccess:false, message:"비밀번호가 틀렸습니다."})
-    
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다.',
+        });
 
       //비밀번호까지 맞다면 토큰을 생성하기.
-      user.generateToken((err, user)=>{
+      user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
 
         // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지에 저장할수있다.
-        res.cookie("x_auth",user.token)
+        res
+          .cookie('x_auth', user.token)
           .status(200)
-          .json({loginSuccess:true, userId: user._id})
-        
-
-      })
-    })
-  })
-})
-
-
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
 
 // 로그인
 
-
-app.get('/api/users/auth', auth, (req, res)=>{
-
+app.get('/api/users/auth', auth, (req, res) => {
   // 여기까지 미들웨어를 통과해 왔다는 얘기는  Authentication이 True라는 말.
   res.status(200).json({
-
-
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
     isAtuth: true,
-    email : req.user.email,
+    email: req.user.email,
     name: req.user.name,
     lastname: req.user.lastname,
     role: req.user.role,
-    image: req.user.image
+    image: req.user.image,
+  });
+});
 
-  })
-
-
-  
-})
-
-
-
-
-
-
-app.get('/api/users/logout', auth, (req,res)=>{
-
-  User.findOneAndUpdate({_id: req.user._id},
-    { token: ""}
-    , (err, user)=>{
-      if (err) return res.json({ success: false, err});
-      return res.status(200).send({
-        success: true
-      })
-    })
-
-})
-
-
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
 
 const port = 8080;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
